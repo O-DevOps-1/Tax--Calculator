ba1ddf21d9cb71e49ec6fbacfdaa9b098db5c091
--omar <ofattahserir@gmail.com>
2024-08-08 20:16:51 +0200

    Create Topic.java

---
+++ backend/Topic.java
@@ -0,0 +1,19 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    @OneToMany(mappedBy = "topic")
+    private List<Post> posts;
+
+    // getters and setters
+  }

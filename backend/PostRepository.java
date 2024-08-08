f36f4821faca02c0509d83957fb9444ae197bb6f
--omar <ofattahserir@gmail.com>
2024-08-08 20:41:06 +0200

    Delete backend directory

--- backend/PostRepository.java
+++
@@ -1,4 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
--- backend/TopicRepository.java
+++
@@ -1,3 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TopicRepository extends JpaRepository<Topic, Long> {}
